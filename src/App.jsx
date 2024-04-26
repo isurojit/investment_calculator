b1e068524f9ebf84649916566d4486fd3de6320f
isurojit <iamsurojit@hotmail.com>
2024-04-26 22:09:00 +0530

    user input component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="mx-auto text-center">
       <Header />
-      {/* <UserInput /> */}
+      <UserInput />
     </div>
   );
 };
--- src/Section/UserInput.jsx
+++ src/Section/UserInput.jsx
@@ -1,49 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 5,
+    duration: 10,
+  });
+
+  //All the functions will be connected
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
   return (
-    <div class="flex flex-wrap -mx-2">
-      <div class="w-full md:w-1/2 px-2 mb-4 md:mb-0">
-        <label for="field1" class="block mb-2">
-          Field 1
-        </label>
-        <input
-          id="field1"
-          type="text"
-          class="w-full md:w-1/2 px-4 py-2 border rounded-md"
-        />
-      </div>
-      <div class="w-full md:w-1/2 px-2 mb-4 md:mb-0">
-        <label for="field2" class="block mb-2">
-          Field 2
-        </label>
-        <input
-          id="field2"
-          type="text"
-          class="w-full md:w-1/2 px-4 py-2 border rounded-md"
-        />
-      </div>
-      <div class="w-full md:w-1/2 px-2">
-        <label for="field3" class="block mb-2">
-          Field 3
-        </label>
-        <input
-          id="field3"
-          type="text"
-          class="w-full md:w-1/2 px-4 py-2 border rounded-md"
-        />
-      </div>
-      <div class="w-full md:w-1/2 px-2">
-        <label for="field4" class="block mb-2">
-          Field 4
-        </label>
-        <input
-          id="field4"
-          type="text"
-          class="w-full md:w-1/2 px-4 py-2 border rounded-md"
-        />
-      </div>
-    </div>
+    <section className="w-full max-w-lg mx-auto mt-4">
+      <form className="form-control">
+        <div className="input-container">
+          <label className="label-style" htmlFor="initialInvestment">
+            Initial Investment
+          </label>
+          <input
+            className="input-style"
+            id="initialInvestment"
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label className="label-style" htmlFor="annualInvestment">
+            Annual Investment
+          </label>
+          <input
+            className="input-style"
+            id="annualInvestment"
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label className="label-style" htmlFor="expectedReturn">
+            Expected Return
+          </label>
+          <input
+            className="input-style"
+            id="expectedReturn"
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label className="label-style" htmlFor="duration">
+            Duration
+          </label>
+          <input
+            className="input-style"
+            id="duration"
+            type="number"
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+            required
+          />
+        </div>
+        <div className="w-full mt-4">
+          <button className="btn-style" type="button">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
